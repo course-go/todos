6bd00a647d3318ff65582be06d7c1531cc6aad66
Stanislav Zeman <zeman@standa.dev>
2024-07-13 14:56:08 +0200

    feat(todos): update json serialization

--- internal/todos/todo.go
+++ internal/todos/todo.go
@@ -9,7 +9,7 @@ import (
 type Todo struct {
 	ID          uuid.UUID  `json:"id,omitempty"`
 	Description string     `json:"description,omitempty"`
-	CompletedAt *time.Time `json:"completed_at,omitempty"`
-	CreatedAt   time.Time  `json:"created_at,omitempty"`
-	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
+	CompletedAt *time.Time `json:"completedAt,omitempty"`
+	CreatedAt   time.Time  `json:"createdAt,omitempty"`
+	UpdatedAt   *time.Time `json:"updatedAt,omitempty"`
 }
