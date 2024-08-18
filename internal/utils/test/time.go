8847a9cfb95b67a731dc6a9128b0362967923f65
Stanislav Zeman <zeman@standa.dev>
2024-08-18 18:00:42 +0200

    test(utils): remove timezone from time factory

--- internal/utils/test/time.go
+++ internal/utils/test/time.go
@@ -10,7 +10,7 @@ import (
 func TimeNow(t *testing.T) ttime.Factory {
 	t.Helper()
 	return func() time.Time {
-		time, err := time.Parse(time.RFC3339Nano, "2024-08-18T14:14:45.847679+02:00")
+		time, err := time.Parse(time.RFC3339Nano, "2024-08-18T12:14:45.847679Z")
 		if err != nil {
 			t.Fatalf("could not parse time: %v", err)
 		}
