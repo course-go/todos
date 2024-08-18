772bd910b44dc8fa1e273a6ccca1365c8e7fe80f
Stanislav Zeman <zeman@standa.dev>
2024-08-18 15:42:37 +0200

    feat(middleware): implement logging

---
+++ internal/controllers/middleware/logging.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+func Logging(logger *slog.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			uri := r.RequestURI
+			method := r.Method
+			next.ServeHTTP(w, r)
+			duration := time.Since(start)
+			logger.Info("handled HTTP request",
+				"uri", uri,
+				"method", method,
+				"duration", duration,
+			)
+		})
+	}
+}
