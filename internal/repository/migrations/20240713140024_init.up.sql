60306f99a3e2421859d8f256958d99c84b60c007
Stanislav Zeman <zeman@standa.dev>
2024-08-18 18:00:06 +0200

    feat(migrations): remove timezones from schema

--- internal/repository/migrations/20240713140024_init.up.sql
+++ internal/repository/migrations/20240713140024_init.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE todos (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   description TEXT NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT Now(),
-  updated_at TIMESTAMPTZ,
-  completed_at TIMESTAMPTZ,
-  deleted_at TIMESTAMPTZ
+  created_at TIMESTAMP NOT NULL DEFAULT Now(),
+  updated_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  deleted_at TIMESTAMP
 );
